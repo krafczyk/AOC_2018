b2b8c5b04f8584b33127d5e3aeb1ef140e406d81
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-07 14:34:11 -0600

    Add task template and script to create new tasks

---
+++ scripts/new_task.sh
@@ -0,0 +1,1 @@
+cp src/task_template.cpp src/task_$1.cpp
---
+++ src/task_template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include "ArgParseStandalone.h"
+#include <string>
+#include <set>
+#include <map>
+#include <sstream>
+#include <limits>
+#include <algorithm>
+
+int main(int argc, char** argv) {
+	// Parse Arguments
+	std::string input_filepath;
+	bool verbose = false;
+	ArgParse::ArgParser Parser("Task Template");
+	Parser.AddArgument("-i/--input", "File defining the input", &input_filepath);
+	Parser.AddArgument("-v/--verbose", "Print Verbose output", &verbose);
+
+	if (Parser.ParseArgs(argc, argv) < 0) {
+		std::cerr << "Problem parsing arguments!" << std::endl;
+		return -1;
+	}
+
+	if (Parser.HelpPrinted()) {
+		return 0;
+	}
+
+	// Open input as stream
+	std::ifstream infile(input_filepath);
+
+	return 0;
+}
