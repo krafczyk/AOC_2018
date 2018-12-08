689227297f8edbe90fb23e0f993604654a3e7d50
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-07 21:04:03 -0600

    Fix copy script

--- scripts/copy_task.sh
+++ scripts/copy_task.sh
@@ -1,3 +1,9 @@
 cp src/task_$1.cpp src/task_$2.cpp
-cp problems/task_$1* problems/task_$2*
-cp tests/task_$1* tests/task_$2*
+for file in `ls problems/task_$1*`; do
+	new_file=`echo ${file} | sed "s/$1/$2/"`
+	cp ${file} ${new_file}
+done;
+for file in `ls tests/task_$1*`; do
+	new_file=`echo ${file} | sed "s/$1/$2/"`
+	cp ${file} ${new_file}
+done;
