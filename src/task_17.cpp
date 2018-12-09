d82c06504f62359628c96efb4c70b06e0e1abbbe
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-09 08:35:39 -0600

    Solve task 17

--- src/task_17.cpp
+++ src/task_17.cpp
@@ -16,6 +16,11 @@ class marble {
 			this->cw = nullptr;
 			this->ccw = nullptr;
 		}
+		~marble() {
+		}
+		void print() const {
+			std::cout << this->ccw->val << " <- " << this->val << " -> " << this->cw->val << std::endl;
+		}
 		int val;
 		marble* cw;
 		marble* ccw;
@@ -34,28 +39,85 @@ class marble_circle {
 		}
 		~marble_circle() {
 			marble* init = this->current_marble;
+			marble* next = this->current_marble->cw;
+			delete this->current_marble;
+			this->current_marble = next;
+			while(this->current_marble != init) {
+				next = this->current_marble->cw;
+				delete this->current_marble;
+				this->current_marble = next;
+			}
+		}
+
+		void insertMarbleCW(marble* existing_marble, marble* new_marble) {
+				// Nothing special, add between the existing marble and next marble cw from
+				// the existing marble.
+				marble* next = existing_marble->cw;
+
+				// Between existing and new
+				existing_marble->cw = new_marble;
+				new_marble->ccw = existing_marble;
+				// Between new and next
+				new_marble->cw = next;
+				next->ccw = new_marble;		
+		}
+
+		int removeMarble(marble* the_marble) {
+			int val = the_marble->val;
+			marble* prev = the_marble->ccw;
+			marble* next = the_marble->cw;
+
+			prev->cw = next;
+			next->ccw = prev;
+
+			delete the_marble;
+			return val;
 		}
 
 		// Returns score from adding the marble.
 		int addMarble() {
-			int new_marble_val = this->last_marble + 1;
-			if (new_marble_val%23 == 0) {
+			// Update value of last marble, a bit misleading, but
+			// this is now the value of the new marble.
+			this->last_marble += 1;
+			if (this->last_marble%23 == 0) {
+				int answer = this->last_marble;
+				answer += removeMarble(this->current_marble->ccw->ccw->ccw->ccw->ccw->ccw->ccw);
+				this->current_marble = this->current_marble->ccw->ccw->ccw->ccw->ccw->ccw;
+				return answer;
 			} else {
 				// Nothing special, add between the 1st and 2nd marble cw from
 				// the current marble.
-				marble* new_marble = new marble(new_marble_val);
-				marble* first_cw = this->current_marble->cw;
-				marble* second_cw = first_cw->cw;
-
-				// Between 1st and new
-				first_cw->cw = new_marble;
-				new_marble->ccw = first_cw;
-				// Between 2nd and new
-				new_marble->cw = second_cw;
-				second_cw->ccw = new_marble;
+				marble* new_marble = new marble(this->last_marble);
+				insertMarbleCW(this->current_marble->cw, new_marble);
+				// Set a new current marble
+				this->current_marble = new_marble;
 				return 0;
 			}
 		}
+
+		void print_state() const {
+			marble* init = this->current_marble;
+			marble* the_marble = init;
+			std::cout << init->val << " ";
+			the_marble = the_marble->cw;
+			while(the_marble != init) {
+				std::cout << the_marble->val << " ";
+				the_marble = the_marble->cw;
+			}
+			std::cout << std::endl;
+		}
+
+		void print_detailed_state() const {
+			marble* init = this->current_marble;
+			marble* the_marble = init;
+			the_marble->print();
+			the_marble = the_marble->cw;
+			while(the_marble != init) {
+				the_marble->print();
+				the_marble = the_marble->cw;
+			}
+		}
+
 	private:
 		marble* current_marble;
 		int last_marble;
@@ -109,6 +171,45 @@ int main(int argc, char** argv) {
 		std::cout << num_players << " players; last marble is worth " << last_val << " points" << std::endl;
 		if (test_mode) {
 			std::cout << "high score should be " << test_result << std::endl;
+		}
+	}
+
+	marble_circle the_game;
+	int scores[num_players];
+	for(int idx=0; idx < num_players; ++idx) {
+		scores[idx] = 0;
+	}
+	int num_turns = 0;
+	int current_player = 0;
+	while(num_turns < last_val) {
+		scores[current_player] += the_game.addMarble();
+		if(verbose) {
+			std::cout << "[" << current_player+1 << "] ";
+			the_game.print_state();
+			the_game.print_detailed_state();
+		}
+		current_player += 1;
+		current_player = current_player % num_players;
+		num_turns += 1;
+	}
+
+	int high_score = std::numeric_limits<int>::min();
+	for(int idx = 0; idx < num_players; ++idx) {
+		if (scores[idx] > high_score) {
+			high_score = scores[idx];
+		}
+	}
+
+	std::cout << num_players << " players; last marble is worth " << last_val << " points" << std::endl;
+	std::cout << "The high score is " << high_score << std::endl;
+
+	if (test_mode) {
+		if(high_score == test_result) {
+			std::cout << "Test Passed!" << std::endl;
+			return 0;
+		} else {
+			std::cout << "Test Failed!" << std::endl;
+			return -1;
 		}
 	}
 
