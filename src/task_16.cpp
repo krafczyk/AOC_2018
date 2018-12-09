146417aed5f749a65ee00f50dd4dc148d4a97383
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-08 20:02:28 -0600

    Complete task 16!

--- src/task_16.cpp
+++ src/task_16.cpp
@@ -10,21 +10,40 @@
 #include "ArgParseStandalone.h"
 #include "utilities.h"
 
-int sum_metadata(std::queue<int>& the_queue) {
+int find_node_value(std::queue<int>& the_queue) {
 	int num_child = the_queue.front();
 	the_queue.pop();
 	int num_meta = the_queue.front();
 	the_queue.pop();
-	// Sum children metadata
+	// Calc value
 	int answer = 0;
-	while(num_child > 0) {
-		answer += sum_metadata(the_queue);
-		--num_child;
-	}
-	while(num_meta > 0) {
-		answer += the_queue.front();
-		the_queue.pop();
-		--num_meta;
+	if (num_child == 0) {
+		while(num_meta > 0) {
+			answer += the_queue.front();
+			the_queue.pop();
+			--num_meta;
+		}
+	} else {
+		std::vector<int> child_costs;
+		int temp_num_child = num_child;
+		while(temp_num_child > 0) {
+			child_costs.push_back(find_node_value(the_queue));
+			--temp_num_child;
+		}
+		while(num_meta > 0) {
+			int meta_val = the_queue.front();
+			the_queue.pop();
+			--num_meta;
+			if (meta_val == 0) {
+				// skip value
+				continue;
+			} else if (meta_val > num_child) {
+				// skip value
+				continue;
+			} else {
+				answer += child_costs[meta_val-1];
+			}
+		}
 	}
 	return answer;
 }
@@ -33,7 +52,7 @@ int main(int argc, char** argv) {
 	// Parse Arguments
 	std::string input_filepath;
 	bool verbose = false;
-	ArgParse::ArgParser Parser("Task 15");
+	ArgParse::ArgParser Parser("Task 16");
 	Parser.AddArgument("-i/--input", "File defining the input", &input_filepath);
 	Parser.AddArgument("-v/--verbose", "Print Verbose output", &verbose);
 
@@ -69,7 +88,7 @@ int main(int argc, char** argv) {
 		license_nums.push(license_numbers[idx]);
 	}
 
-	std::cout << "The sum of all meta data entries is: " << sum_metadata(license_nums) << std::endl;
+	std::cout << "The value of the root node is: " << find_node_value(license_nums) << std::endl;
 
 	return 0;
 }
