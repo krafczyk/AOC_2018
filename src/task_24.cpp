2f2c162dc095271da333c0879af45ced8066f686
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-12 00:14:02 -0600

    progress on task 24

--- src/task_24.cpp
+++ src/task_24.cpp
@@ -10,8 +10,8 @@
 #include "ArgParseStandalone.h"
 #include "utilities.h"
 
-void print_state(bool* state, int size) {
-	for(int idx=0; idx < size; ++idx) {
+void print_state(bool* state, long size) {
+	for(long idx=0; idx < size; ++idx) {
 		if(state[idx]) {
 			std::cout << '#';
 		} else {
@@ -24,12 +24,12 @@ void print_state(bool* state, int size) {
 class rule {
 	public:
 		rule(bool* the_rule) {
-			for(int i=0; i<5; ++i) {
+			for(long i=0; i<5; ++i) {
 				this->the_rule[i] = the_rule[i];
 			}
 		}
 		rule(std::string& string_rule) {
-			for(int i=0; i<5; ++i) {
+			for(long i=0; i<5; ++i) {
 				if(string_rule[i] == '#') {
 					the_rule[i] = true;
 				} else {
@@ -37,9 +37,9 @@ class rule {
 				}
 			}
 		}
-		bool match(bool* state, int center) const {
+		bool match(bool* state, long center) const {
 			bool matches = true;
-			for(int i=0; i<5; ++i) {
+			for(long i=0; i<5; ++i) {
 				if(state[center+i-2] != the_rule[i]) {
 					matches = false;
 					break;
@@ -48,7 +48,7 @@ class rule {
 			return matches;
 		}
 		void print() const {
-			for(int i=0; i<5; ++i) {
+			for(long i=0; i<5; ++i) {
 				if(the_rule[i]) {
 					std::cout << '#';
 				} else {
@@ -64,7 +64,7 @@ int main(int argc, char** argv) {
 	// Parse Arguments
 	std::string input_filepath;
 	bool verbose = false;
-	ArgParse::ArgParser Parser("Task 23");
+	ArgParse::ArgParser Parser("Task 24");
 	Parser.AddArgument("-i/--input", "File defining the input", &input_filepath);
 	Parser.AddArgument("-v/--verbose", "Print Verbose output", &verbose);
 
@@ -87,25 +87,26 @@ int main(int argc, char** argv) {
 	std::string init_state = line.substr(15);
 
 	// Helper variables and function to transition
-	int left_extent = -30;
-	int right_extent = (int)(line.size()*1.2);
-	int num_places = -left_extent+right_extent+1;
-	auto idx_to_num [[maybe_unused]] = [left_extent](int idx) {
+	long left_extent = -5000000000;
+	//long right_extent = (long)(line.size()*1);
+	long right_extent = 75000000000;
+	long num_places = -left_extent+right_extent+1;
+	auto idx_to_num = [left_extent](long idx) {
 		return idx+left_extent;
 	};
-	auto num_to_idx = [left_extent](int num) {
+	auto num_to_idx = [left_extent](long num) {
 		return num-left_extent;
 	};
 
 	// initialize state
 	bool* state = new bool[num_places];
 	bool* state_next = new bool[num_places];
-	for(int i=0; i < num_places; ++i) {
+	for(long i=0; i < num_places; ++i) {
 		state[i] = false;
 		state_next[i] = false;
 	}
 
-	for(unsigned int idx = 0; idx < init_state.size(); ++idx) {
+	for(unsigned long idx = 0; idx < init_state.size(); ++idx) {
 		if (init_state[idx] == '#') {
 			state[num_to_idx(idx)] = true;
 		}
@@ -125,7 +126,7 @@ int main(int argc, char** argv) {
 
 	if(verbose) {
 		std::cout << "Read in rules are:" << std::endl;
-		for(unsigned int idx=0; idx < rules.size(); ++idx) {
+		for(unsigned long idx=0; idx < rules.size(); ++idx) {
 			rules[idx].print();
 		}
 	}
@@ -134,10 +135,10 @@ int main(int argc, char** argv) {
 		std::cout << std::setw(3) << std::setfill(' ') << 0 << ": ";
 		print_state(state, num_places);
 	}
-	int num_gen = 20;
-	int step = 0;
+	long num_gen = 50000000000;
+	long step = 0;
 	while(step < num_gen) {
-		for(int i=2; i<num_places-2; ++i) {
+		for(long i=2; i<num_places-2; ++i) {
 			bool rule_match = false;
 			for(auto rule_it = rules.begin(); rule_it != rules.end(); ++rule_it) {
 				if (rule_it->match(state, i)) {
@@ -162,8 +163,8 @@ int main(int argc, char** argv) {
 		++step;
 	}
 
-	int pot_sum = 0;
-	for(int i=0; i< num_places; ++i) {
+	long pot_sum = 0;
+	for(long i=0; i< num_places; ++i) {
 		if(state[i]) {
 			pot_sum += idx_to_num(i);
 		}
