7abf41800c4504d286d62e2e2dcc01f449832206
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-16 15:30:24 -0600

    Add script to run task_29 tests

---
+++ scripts/run_task_29_tests.sh
@@ -0,0 +1,5 @@
+./bin/task_29 -i tests/task_29_2.txt -t 36334
+./bin/task_29 -i tests/task_29_3.txt -t 39514
+./bin/task_29 -i tests/task_29_4.txt -t 27755
+./bin/task_29 -i tests/task_29_5.txt -t 28944
+./bin/task_29 -i tests/task_29_6.txt -t 18740
