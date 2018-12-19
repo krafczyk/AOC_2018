6bbf34182e73f8cde3f570da255a45c416ec6684
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-19 00:34:28 -0600

    Add script to run task 30 tasks

---
+++ scripts/run_task_30_tests.sh
@@ -0,0 +1,5 @@
+./bin/task_30 -i tests/task_30_1.txt -t 4988
+./bin/task_30 -i tests/task_30_2.txt -t 31284
+./bin/task_30 -i tests/task_30_3.txt -t 3478
+./bin/task_30 -i tests/task_30_4.txt -t 6474
+./bin/task_30 -i tests/task_30_5.txt -t 1140
