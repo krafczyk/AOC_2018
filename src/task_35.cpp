b068a77d1f80552c05351688f8d69429ca384b3c
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-22 13:38:50 -0600

    Solve task 35

--- src/task_35.cpp
+++ src/task_35.cpp
@@ -11,7 +11,8 @@
 
 class lumberyard_state {
     public:
-        lumberyard_state() {
+        lumberyard_state(int in_size) {
+            this->size = in_size;
             lumberyard = new char[size*size];
             for(int idx = 0; idx < size*size; ++idx) {
                 lumberyard[idx] = 0;
@@ -30,6 +31,10 @@ class lumberyard_state {
             std::map<char,int> the_map;
             for(int i_diff = -1; i_diff <= 1; ++i_diff) {
                 for(int j_diff = -1; j_diff <= 1; ++j_diff) {
+                    if((j_diff == 0)&&(i_diff == 0)) {
+                        // Skip the middle.
+                        continue;
+                    }
                     int i_idx = i+i_diff;
                     int j_idx = j+j_diff;
                     if((i_idx >= 0)&&(j_idx >= 0)&&
@@ -49,7 +54,7 @@ class lumberyard_state {
             }
         }
     private:
-        static const int size = 50;
+        int size;
         char* lumberyard;
 };
 
@@ -57,9 +62,15 @@ int main(int argc, char** argv) {
 	// Parse Arguments
 	std::string input_filepath;
 	bool verbose = false;
+    bool super_verbose = false;
+    int size = 50;
+    int num_minutes = 0;
 	ArgParse::ArgParser Parser("Task 35");
 	Parser.AddArgument("-i/--input", "File defining the input", &input_filepath);
+    Parser.AddArgument("-m/--minutes", "Specify number of minutes", &num_minutes);
+    Parser.AddArgument("-s/--size", "Specify size", &size, ArgParse::Argument::Optional);
 	Parser.AddArgument("-v/--verbose", "Print Verbose output", &verbose);
+    Parser.AddArgument("-sv/--super-verbose", "Print Extra Verbose output", &super_verbose);
 
 	if (Parser.ParseArgs(argc, argv) < 0) {
 		std::cerr << "Problem parsing arguments!" << std::endl;
@@ -71,8 +82,8 @@ int main(int argc, char** argv) {
 	}
 
     // Create Lumberyards
-    lumberyard_state* current_lumberyard = new lumberyard_state();
-    lumberyard_state* next_lumberyard [[maybe_unused]]= new lumberyard_state();
+    lumberyard_state* current_lumberyard = new lumberyard_state(size);
+    lumberyard_state* next_lumberyard [[maybe_unused]]= new lumberyard_state(size);
 
 	// Open input as stream
 	std::ifstream infile(input_filepath);
@@ -89,6 +100,70 @@ int main(int argc, char** argv) {
         std::cout << "Initial State:" << std::endl;
         current_lumberyard->print(std::cout);
     }
+
+    int minutes = 0;
+    while(minutes < num_minutes) {
+        // Get next lumberyard
+        for(int line_idx = 0; line_idx < size; ++line_idx) {
+            for(int x_idx = 0; x_idx < size; ++x_idx) {
+                auto neighbor_stats = current_lumberyard->neighbor_stats(line_idx, x_idx);
+                char tile = (*current_lumberyard)(line_idx,x_idx);
+                if(tile == '.') {
+                    if(neighbor_stats['|'] >= 3) {
+                        next_lumberyard->assign(line_idx, x_idx) = '|';
+                    } else {
+                        next_lumberyard->assign(line_idx, x_idx) = '.';
+                    }
+                } else if (tile == '|') {
+                    if(neighbor_stats['#'] >= 3) {
+                        next_lumberyard->assign(line_idx, x_idx) = '#';
+                    } else {
+                        next_lumberyard->assign(line_idx, x_idx) = '|';
+                    }
+                } else if (tile == '#') {
+                    if((neighbor_stats['|'] >= 1)&&(neighbor_stats['#']>=1)) {
+                        next_lumberyard->assign(line_idx, x_idx) = '#';
+                    } else {
+                        next_lumberyard->assign(line_idx, x_idx) = '.';
+                    }
+                } else {
+                    throw std::runtime_error("Unknown tile encountered!");
+                }
+            }
+        }
+        // Swap pointers
+        lumberyard_state* temp = current_lumberyard;
+        current_lumberyard = next_lumberyard;
+        next_lumberyard = temp;
+        // Increment and display info.
+        minutes += 1;
+        if(super_verbose) {
+            std::cout << "Minute " << minutes << std::endl;
+            current_lumberyard->print(std::cout);
+        }
+    }
+
+    if(verbose) {
+        std::cout << "Final State:" << std::endl;
+        current_lumberyard->print(std::cout);
+    }
+
+    // Calculate resource value:
+    int num_lumberyards = 0;
+    int num_woods = 0;
+    for(int line_idx = 0; line_idx < size; ++line_idx) {
+        for(int x_idx = 0; x_idx < size; ++x_idx) {
+            char tile = (*current_lumberyard)(line_idx,x_idx);
+            if(tile == '|') {
+                num_woods += 1;
+            }
+            if(tile == '#') {
+                num_lumberyards += 1;
+            }
+        }
+    }
+
+    std::cout << "Resource Value is: " << num_lumberyards*num_woods << std::endl;
 
 	return 0;
 }
