ece6baa8d374f46ce6ea893f49429e032fb1acbc
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-01-02 17:26:53 -0600

    Partial fix for combine.

--- src/task_39.cpp
+++ src/task_39.cpp
@@ -15,11 +15,16 @@ std::vector<std::string> combine(const std::vector<std::vector<std::string>>& li
     std::vector<std::string> results;
     for(auto list_it = list.cbegin(); list_it != list.cend(); ++list_it) {
         std::vector<std::string> new_results;
-        for(size_t o_idx = 0; o_idx < results.size(); ++o_idx) {
-            for(size_t l_idx = 0; l_idx < list_it->size(); ++l_idx) {
-                // Concatenate.
-                new_results.push_back(results[o_idx]+(*list_it)[l_idx]);
+        if(results.size() > 0) {
+            for(size_t o_idx = 0; o_idx < results.size(); ++o_idx) {
+                std::cout << "level 1" << std::endl;
+                for(size_t l_idx = 0; l_idx < list_it->size(); ++l_idx) {
+                    std::cout << "level 2" << std::endl;
+                    // Concatenate.
+                    new_results.push_back(results[o_idx]+(*list_it)[l_idx]);
+                }
             }
+        } else {
         }
         std::swap(results, new_results);
     }
@@ -38,6 +43,18 @@ void remove_duplicates(std::vector<std::string>& in) {
     }
 }
 
+void print_options(const std::vector<std::vector<std::vector<std::string>>>& options, std::ostream& out) {
+    for(auto it_1 = options.cbegin(); it_1 != options.cend(); ++it_1) {
+        out << "Layer 1 item" << std::endl;
+        for(auto it_2 = it_1->cbegin(); it_2 != it_1->cend(); ++it_2) {
+            out << "Layer 2 item" << std::endl;
+            for(auto it_3 = it_2->cbegin(); it_3 != it_2->cend(); ++it_3) {
+                out << *it_3 << std::endl;
+            }
+        }
+    }
+}
+
 // The result of this function is a vector with all options expanded.
 std::vector<std::string> expand_regex(std::string& regex_line, const int stack_level = 0) {
     std::cout << "expand_regex: (" << regex_line << ") (" << stack_level << ")" << std::endl;
@@ -48,6 +65,7 @@ std::vector<std::string> expand_regex(std::string& regex_line, const int stack_l
     options.rbegin()->rbegin()->push_back("");
     while(regex_line.size() != 0) {
         std::cout << "Main loop" << std::endl;
+        print_options(options, std::cout);
         if((regex_line[0] == '^')||(regex_line[0] == '$')) {
             std::cout << "branch 1" << std::endl;
             // Skip these characters
@@ -85,6 +103,10 @@ std::vector<std::string> expand_regex(std::string& regex_line, const int stack_l
     while(options.size() != 0) {
         // perform multiplexing of strings
         std::vector<std::string> option_list = combine(options[0]);
+        std::cout << "Combined option list:" << std::endl;
+        ConstForEach(option_list, [](const std::string& in) {
+            std::cout << in << std::endl;
+        });
         // Add to resulting options.
         resulting_options.insert(resulting_options.end(), option_list.begin(), option_list.end());
         // Remove the element from options
