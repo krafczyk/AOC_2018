b8f51c9b2235237e2212372bc6c5e2c7190f38e0
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-01-02 21:11:39 -0600

    Add scripts

---
+++ scripts/run_task_39_tests.sh
@@ -0,0 +1,5 @@
+./bin/task_39 -i tests/task_39_1.txt -t 3
+./bin/task_39 -i tests/task_39_2.txt -t 10
+./bin/task_39 -i tests/task_39_3.txt -t 18
+./bin/task_39 -i tests/task_39_4.txt -t 23
+./bin/task_39 -i tests/task_39_5.txt -t 31
