4e6e1a1be17103b7eab8706659f6f85ec68c4669
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-05-20 13:11:15 -0500

    Add long data type version of the pair hash

--- utilities/utilities.h
+++ utilities/utilities.h
@@ -187,3 +187,12 @@ int pair_hash(int x, int y) {
     int C = (int)((A >= B ? A*A+A+B : A+B*B)/2);
     return ((x < 0 && y < 0) || (x >= 0 && y >= 0) ? C : -C - 1);
 }
+
+long pair_hash_l(long x, long y) {
+    // Here we use something similar to cantor pairing.
+    // https://stackoverflow.com/questions/919612/mapping-two-integers-to-one-in-a-unique-and-deterministic-way
+    unsigned long A = (x >= 0 ? 2*x : -2*x-1);
+    unsigned long B = (y >= 0 ? 2*y : -2*y-1);
+    long C = (long)((A >= B ? A*A+A+B : A+B*B)/2);
+    return ((x < 0 && y < 0) || (x >= 0 && y >= 0) ? C : -C - 1);
+}
